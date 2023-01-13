ba615b952beb5d12971efeb32f26ce10b6d94da6
Fuseman <vmohan2211@gmail.com>
2023-01-13 06:54:50 -0800

    Billing (#1)

--- assets/js/chainauth/billing.js
+++ assets/js/chainauth/billing.js
@@ -5,6 +5,7 @@
 * we should call ^ before loading this script
 */
 
+const freeTryButton = "#freeTry";
 const caskOnRiseButton = "#ontherisePlan";
 const caskTakeOffButton = "#takeoffPlan";
 
@@ -14,12 +15,19 @@ const sentMailKey = "emailCalls";
 const maxAPIKey = "apiLimit";
 const maxEmailKey = "emailLimit";
 
+const onTheRisePlanId = "2938218295";
+const takeOffPlanId = "3666810638";
+
 jQuery(window).on("auth.loaded", async function (e) {
+  jQuery(caskOnRiseButton).attr( "plan", onTheRisePlanId  );
+  jQuery(caskTakeOffButton).attr( "plan", takeOffPlanId );
+
   jQuery(caskOnRiseButton).attr( "ref", getTokenValue(userIdKey)  );
   jQuery(caskTakeOffButton).attr( "ref", getTokenValue(userIdKey) );
 
   var details = await GetAppDetails();
   updateDetail(details);
+  updateSubOptions(details);
 });
 
 async function GetAppDetails() {
@@ -44,5 +52,28 @@ function updateDetail(details) {
     jQuery("#sentMail").text(details[sentMailKey]);
     jQuery("#maxEmail").text(details[maxEmailKey]);
     jQuery("#emailBar").css("width", (details[sentMailKey] / details[maxEmailKey] * 100) + "%");
+  }
+}
+
+function updateSubOptions(details) {
+  var id = "0x" + BigInt(details.subId).toString(16);
+
+  if (details.planId == onTheRisePlanId) {
+    let caskOnRise = jQuery( freeTryButton ).clone().appendTo( jQuery(caskOnRiseButton).parent() );
+    caskOnRise.text("Your Plan");
+    caskOnRise.attr("href", "https://app.cask.fi/#/flow/subscription/" + id);
+
+    jQuery(caskOnRiseButton).remove();
+    jQuery(caskTakeOffButton).attr("disabled", "disabled");
+  } else if (details.planId == takeOffPlanId) {
+
+    let takeoffPlan = jQuery( freeTryButton ).clone().appendTo( jQuery(caskTakeOffButton).parent() );
+    takeoffPlan.text("Your Plan");
+    takeoffPlan.attr("href", "https://app.cask.fi/#/flow/subscription/" + id);
+
+    jQuery(caskTakeOffButton).remove();
+    jQuery(caskOnRiseButton).attr("disabled", "disabled");
+  } else if (details.planId.includes("free")) {
+    jQuery(freeTryButton).text("Your Plan");
   }
 }
